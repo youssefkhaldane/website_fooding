fe3c70326d2662a8e084ce55c61596812186e8ea
youssefkhaldane <155687055+youssefkhaldane@users.noreply.github.com>
2024-02-28 23:33:49 +0100

    Add files via upload

--- product.js
+++ product.js
@@ -1,3 +1,23 @@
-function toggleFavorite(icon) {
-    icon.classList.toggle('active');
+// function changecolor() {
+//     document.getElementById("icons").classList.toggle("red-heart");
+// }
+
+// Yasser code 
+const icons = document.querySelectorAll(".icons");
+// console.log(icons)
+function handleFavori(index){
+    // Get the reference to the element
+    var icon = icons[index];
+
+    // Toggle the "red-heart" class
+    icon.classList.toggle("red-heart");
+
+    // Set the base class "icons fa-solid fa-heart" if "red-heart" is not present, otherwise remove it
+    if (icon.classList.contains("red-heart")) {
+        console.log('im in')
+        icon.className = "icons fa-solid fa-heart red-heart";
+    }
+    else{
+        icon.className = "icons fa-regular fa-heart";
+    }
 }
